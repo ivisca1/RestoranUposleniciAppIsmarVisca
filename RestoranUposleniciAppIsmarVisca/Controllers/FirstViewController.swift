f7b67a831467ad1a748a51146d9ba65137748e2d
Ismar Višća <ivisca1@etf.unsa.ba>
2023-06-30 17:08:57 +0200

    log in, sign up design

---
+++ RestoranUposleniciAppIsmarVisca/Controllers/FirstViewController.swift
@@ -0,0 +1,26 @@
+//
+//  FirstViewController.swift
+//  RestoranUposleniciAppIsmarVisca
+//
+//  Created by User on 30. 6. 2023..
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var logInButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func logInButtonPressed(_ sender: UIButton) {
+    }
+    
+
+    @IBAction func signUpButtonPressed(_ sender: UIButton) {
+    }
+}
---
+++ RestoranUposleniciAppIsmarVisca/Controllers/LogInViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LogInViewController.swift
+//  RestoranUposleniciAppIsmarVisca
+//
+//  Created by User on 30. 6. 2023..
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    @IBOutlet weak var logInButton: UIButton!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func logInButtonPressed(_ sender: UIButton) {
+    }
+    
+}
